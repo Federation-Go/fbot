fc17759337cb3788e8f497238c9a68876472321d
mo.liu <mo.liu@gameloft.org>
2015-12-02 17:27:29 +0800

    update base

--- model/base.py
+++ model/base.py
@@ -8,15 +8,18 @@ class Base(object):
 
 	def __getattr__(self,name):
 		if name in self.m:
-			return functools.partial(self.request,name=name)
+			return functools.partial(self.request,name)
 		else:
 			raise AttributeError
 	def request(self,name,pandora,*args,**kwargs):
-		url=locate.locate(pandora,self.model)
+		if pandora not in locate.pandora_url:
+			url=locate.locate(pandora,self.model)
+		else:
+			url=locate.pandora_url[pandora]
 		method,addr=self.m[name]
 		addr=addr % kwargs
 		url='https://%s%s' % (url,addr)
 		req=requests.Request(method,url,params=kwargs)
-                r=req.prepare()
-                s=requests.Session()
+		r=req.prepare()
+		s=requests.Session()
 		return s.send(r)

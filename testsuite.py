fb1baa37c87982a94a8bc48e8ceaf98666562774
weijian.guo <weijian.guo@gameloft.com>
2015-12-03 11:34:33 +0800

    + Implement a simple handler which could parse and run test

--- testsuite.py
+++ testsuite.py
@@ -1,3 +1,23 @@
-from handler import *
-from model import*
-print 1
+import json
+import argparse
+import os
+import os.path
+from handler import testcase
+import sys
+import glob
+
+path=os.path.dirname(os.path.realpath(__file__))
+os.chdir(path)
+
+def main(*args,**kwargs):
+    conf=json.load(open("config/config.json",'r'))
+    test_dir=getattr(conf,'test_dir',"testcases")
+    testcases=glob.glob("%s/*.test"%test_dir)
+    for i in testcases:
+        handler=testcase.Testcase(conf['pandora'],i)
+        print handler.run_test()
+
+    
+
+if __name__=="__main__":
+    main()
